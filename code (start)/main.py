6b0f45f2157d3c28b365e2ac806804bc4eaea293
Nikolas Ernst <148749813+NikolasErnst@users.noreply.github.com>
2025-01-09 15:54:10 +0100

    Objects und Layer (auch top layer)

--- code (start)/main.py
+++ code (start)/main.py
@@ -21,12 +21,22 @@ class Game:
         self.setup(self.tmx_maps["world"], "house")
 
     def import_assets(self):
-        self.tmx_maps = {"world": load_pygame(join("..", "data", "maps", "world.tmx"))}
+        self.tmx_maps = {
+            "world": load_pygame(join("..", "data", "maps", "world.tmx")),
+            "hospital": load_pygame(join("..", "data", "maps", "hospital.tmx")),
+        }
 
     def setup(self, tmx_map, player_start_pos):
-        for x, y, surf in tmx_map.get_layer_by_name("Terrain").tiles():
-            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+        # Terrain und Tarrain top Setup
+        for layer in ["Terrain", "Terrain Top"]:
+            for x, y, surf in tmx_map.get_layer_by_name(layer).tiles():
+                Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
 
+        # objects
+        for obj in tmx_map.get_layer_by_name("Objects"):
+            Sprite((obj.x, obj.y), obj.image, self.all_sprites)
+
+        # entities
         for obj in tmx_map.get_layer_by_name("Entities"):
             if obj.name == "Player" and obj.properties["pos"] == player_start_pos:
                 self.player = Player((obj.x, obj.y), self.all_sprites)
@@ -40,9 +50,9 @@ class Game:
                     pygame.quit()
                     exit()
 
-            # game logic 
+            # game logic
             self.all_sprites.update(dt)
-            self.display_surface.fill('black')
+            self.display_surface.fill("black")
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
 

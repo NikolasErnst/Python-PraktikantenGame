cae2c6444cfdd90e938f7676374044ca5e25964a
LoryxOderSo <loryx2@web.de>
2025-01-09 22:29:13 +0100

    Add files via upload

---
+++ data/tilesets/4.Gen._HGSS_Gaia(1).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="4.Gen._HGSS_Gaia(1)" tilewidth="64" tileheight="64" tilecount="2961" columns="47">
+ <image source="../../4.Gen._HGSS_Gaia(1).png" width="3008" height="4044"/>
+</tileset>

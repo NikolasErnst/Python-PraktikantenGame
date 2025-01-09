f08e7acd46b5db70cd94c2d0180a7efe0ed160bc
Nikolas Ernst <148749813+NikolasErnst@users.noreply.github.com>
2025-01-09 19:57:15 +0100

    Player Laufanimation Fertig

--- code (start)/entities.py
+++ code (start)/entities.py
@@ -5,20 +5,32 @@ from settings import *
 class Entity(pygame.sprite.Sprite):
     def __init__(self, pos, frames, groups):
         super().__init__(groups)
-
-        # graphics
+        self.z = WORLD_LAYERS["main"]
+        #char grafik für richtige richtung animation 
         self.frame_index, self.frames = 0, frames
-        
-        # self.direction = vector()
-        # self.speed = 250
-        # self.blocked = False
-        
-        # sprite setup
-        self.image = self.frames['down'][self.frame_index]
+        self.facing_direction = 'down'
+        #movement
+        self.direction = vector()
+        self.speed = 250
+        #sprite setup
+        self.image = self.frames[self.get_state()][self.frame_index]
         self.rect = self.image.get_frect(center=pos)
-        # self.hitbox = self.rect.inflate(-self.rect.width / 2, -60)
 
-        # self.y_sort = self.rect.centery
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[self.get_state()][
+            int(self.frame_index % len(self.frames[self.get_state()]))
+        ]
+    #abfrage ob gelaufen wird oder idle 
+    def get_state(self):
+        moving = bool(self.direction)
+        if moving:
+            if self.direction.x != 0:
+                self.facing_direction = "right" if self.direction.x > 0 else "left"
+            if self.direction.y != 0:
+                self.facing_direction = "down" if self.direction.y > 0 else "up"
+        return f"{self.facing_direction}{'' if moving else '_idle'}"
+
 
 
 # unser basic Player
@@ -51,3 +63,4 @@ class Player(Entity):
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)

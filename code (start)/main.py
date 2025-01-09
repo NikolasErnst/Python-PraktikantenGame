213a9697ffa5de54ef0c1ffb351b57eb201de16d
Nikolas Ernst <148749813+NikolasErnst@users.noreply.github.com>
2025-01-09 11:23:36 +0100

    Sprite Implementiert

--- code (start)/main.py
+++ code (start)/main.py
@@ -1,26 +1,30 @@
 from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
+from sprites import Sprite
+
+
 class Game:
     # generell
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Atruvimon')
+        pygame.display.set_caption("Atruvimon")
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
 
         self.import_assets()
-        self.setup(self.tmx_maps['world'],'house')
+        self.setup(self.tmx_maps["world"], "house")
 
     def import_assets(self):
-        self.tmx_maps = {'world': load_pygame(join('..','data','maps','world.tmx'))}
-
+        self.tmx_maps = {"world": load_pygame(join("..", "data", "maps", "world.tmx"))}
 
     def setup(self, tmx_map, player_start_pos):
-        for x,y,surf in  tmx_map.get_layer_by_name('Terrain').tiles():
-            print(x* TILE_SIZE,y *TILE_SIZE,surf)
-            
-        
-    # loop 
+        for x, y, surf in tmx_map.get_layer_by_name("Terrain").tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
+
+    # loop
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -29,9 +33,10 @@ class Game:
                     exit()
 
             # logik
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     game = Game()
     game.run()

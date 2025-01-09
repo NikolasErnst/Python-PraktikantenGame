f594fe30321af34d042499824909be8d6b70446a
Nikolas Ernst <148749813+NikolasErnst@users.noreply.github.com>
2025-01-09 11:05:11 +0100

    Update main.py
    Initial Main.py

--- code (start)/main.py
+++ code (start)/main.py
@@ -1,1 +1,37 @@
 from settings import *
+from pytmx.util_pygame import load_pygame
+from os.path import join
+class Game:
+    # generell
+    def __init__(self):
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Atruvimon')
+
+        self.import_assets()
+        self.setup(self.tmx_maps['world'],'house')
+
+    def import_assets(self):
+        self.tmx_maps = {'world': load_pygame(join('..','data','maps','world.tmx'))}
+
+
+    def setup(self, tmx_map, player_start_pos):
+        for x,y,surf in  tmx_map.get_layer_by_name('Terrain').tiles():
+            print(x* TILE_SIZE,y *TILE_SIZE,surf)
+            
+        
+    # loop 
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            # logik
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
